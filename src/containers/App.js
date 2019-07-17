b722c6da2bd9f1bad7a9a5fdcf550c4eb5f35a8e
michalkruk <michal.k.kruk@gmail.com>
2019-07-17 23:37:24 +0200

    add simple error boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll.js";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends Component {
     constructor() {
@@ -35,7 +36,9 @@ class App extends Component {
                 <h1 className="title">Users list</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList users={filteredUsers} />
+                    <ErrorBoundry>
+                        <CardList users={filteredUsers} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         );
