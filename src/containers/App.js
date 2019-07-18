38df30ed606f15deef5e8eb5dadc6344bb523883
michalkruk <michal.k.kruk@gmail.com>
2019-07-18 17:25:46 +0200

    up

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll.js";
 import ErrorBoundry from "../components/ErrorBoundry";
+import { setSearchField } from "../actions";
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onSearchChange: event => dispatch(setSearchField(event.target.value))
+    };
+};
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            users: [],
-            searchfield: ""
+            users: []
         };
     }
 
@@ -20,21 +33,18 @@ class App extends Component {
             .then(users => this.setState({ users: users }));
     }
 
-    onSearchChange = e => {
-        this.setState({ searchfield: e.target.value });
-    };
-
     render() {
-        const { users, searchfield } = this.state;
+        const { users } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredUsers = users.filter(user =>
             user.name
                 .toLocaleLowerCase()
-                .includes(searchfield.toLocaleLowerCase())
+                .includes(searchField.toLocaleLowerCase())
         );
         return (
             <div className="tc">
                 <h1 className="title">Users list</h1>
-                <SearchBox searchChange={this.onSearchChange} />
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundry>
                         <CardList users={filteredUsers} />
@@ -45,4 +55,7 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import tachyons from "tachyons";
 import * as serviceWorker from "./serviceWorker";
 import App from "./containers/App";
+import { searchUsers } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(searchUsers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
